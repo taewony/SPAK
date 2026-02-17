a0af08f5214f789df883c47fbcfbe73f8b454bbc
taewony <engchat@gmail.com>
2026-02-17 20:28:00 +0900

    Merge branch 'main' of https://github.com/taewony/SPAK

--- dp.py
+++ dp.py
@@ -3,6 +3,12 @@ import torch
 x = torch.tensor([0.0], requires_grad=True)
 optimizer = torch.optim.SGD([x], lr=0.1)
 
+# 옵티마이저가 현재 저장된 기울기(x.grad)를 사용하여 파라미터를 업데이트합니다.
+# SGD의 업데이트 규칙: x = x - (lr * x.grad)
+# 예를 들어 첫 스텝에서 x=0, grad=-10, lr=0.1이면 x는 
+# x = 0 - (0.1 * -10) = 1로 업데이트됩니다.
+# 이 과정을 반복하면서 점차 최솟점인 x=5에 가까워집니다.
+
 for step in range(30):
     # 함수 정의: y = (x - 5)^2 + 10
     y = (x - 5)**2 + 10
